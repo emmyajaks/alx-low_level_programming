04c932ec9062eddfd2b7c9980fad1495d5c2bc20
emmyajaks <emmyajaks2016@gmail.com>
2023-06-26 09:57:08 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - checks the lenght of the string
+ * @s: string to be checked
+ * Return: The lenght of the string
+ */
+
+int _strlen(char *s)
+{
+	int string_lenght = 0;
+
+	while (s[string_lenght])
+		string_lenght++;
+	return (string_lenght);
+}
