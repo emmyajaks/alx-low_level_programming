7f0b0bbf564040845cdf005e312badbc7701bf23
emmyajaks <emmyajaks2016@gmail.com>
2023-07-06 04:57:30 -0700

    101-wildcmp

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * wildcmp - Compare strings
+ * @s1: pointer to string params
+ * @s2: pointer to string params
+ * Return: 0
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '\0')
+			return (1);
+		{
+			if (*s1 == '\0')
+				return (wildcmp(s1, (s2 + 1)));
+		}
+		return (wildcmp((s1 + 1), s2) || wildcmp(s1, (s2 + 1)));
+	}
+	{
+	if (*s1 == '\0' || *s2 == '\0')
+		return (*s1 == *s2);
+	}
+	{
+		if (*s1 == *s2)
+			return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
+}
